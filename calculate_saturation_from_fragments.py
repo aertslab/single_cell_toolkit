6403d52e4c05b17cd2e0fd6157485586b227763a
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2021-07-27 14:27:29 +0200

    Set better sampling fractions in calculate_saturation_from_fragments.py
    Set better sampling fractions in calculate_saturation_from_fragments.py
    There should be more samplings at the end to improve curve fitting
    instead of a lot of samplings at the start.

--- calculate_saturation_from_fragments.py
+++ calculate_saturation_from_fragments.py
@@ -31,12 +31,12 @@ logger = logging.getLogger('calculate_saturation_from_fragments')
 # Fractions for which to sample from the fragments file:
 #     [
 #         float(f'{sampling_fraction:0.2f}')
-#         for sampling_fraction in list(np.concatenate((np.arange(0.0, 0.1, 0.01), np.arange(0.1, 0.5, 0.05), np.arange(0.5, 1.01, 0.1))))
+#         for sampling_fraction in list(np.concatenate((np.arange(0.0, 0.5, 0.1), np.arange(0.5, 0.9, 0.05), np.arange(0.9, 1.01, 0.02))))
 #     ]
 sampling_fractions_default = [
-    0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-    0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
-    0.5, 0.6, 0.7, 0.8, 0.9, 1.0
+    0.0, 0.1, 0.2, 0.3, 0.4,
+    0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85,
+    0.9, 0.92, 0.94, 0.96, 0.98, 1.0
 ]
 
 
