599bd9ecf18caea51440dc62445b62ceb3d45ae1
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2023-01-05 15:20:08 +0100

    Write only FASTQ read name lines with SAM tags when correcting barcodes.
    Write only FASTQ read name lines with SAM tags when correcting
    barcodes instead of the whole FASTQ file as in a second step
    all this info needs to be added to R1 and R2 reads in the read
    name comment section anyway.

--- extract_and_correct_scalebio_atac_barcode_from_fastq.sh
+++ extract_and_correct_scalebio_atac_barcode_from_fastq.sh
@@ -5,20 +5,20 @@ set -o pipefail
 
 
 
-correct_barcode_in_fastq () {
-    local bc_whitelist_filename="${1}";
+extract_and_correct_scalebio_atac_barcode_from_fastq () {
+    local tenx_atac_bc_whitelist_filename="${1}";
     local fastq_with_raw_bc_filename="${2}";
-    local fastq_with_corrected_bc_filename="${3}";
+    local corrected_bc_filename="${3}";
     local max_mismatches="${4:-1}";
     local min_frac_bcs_to_find="${5:-0.5}";
 
     if [ ${#@} -lt 3 ] ; then
-        printf 'Usage: correct_barcode_in_fastq bc_whitelist_file fastq_with_raw_bc_file fastq_with_corrected_bc_file [max_mismatches] [min_frac_bcs_to_find]\n';
+        printf 'Usage: extract_and_correct_scalebio_atac_barcode_from_fastq tenx_atac_bc_whitelist_file fastq_with_raw_bc_file corrected_bc_file [max_mismatches] [min_frac_bcs_to_find]\n';
         return 1;
     fi
 
-    if [ ! -e "${bc_whitelist_filename}" ] ; then
-        printf 'Error: Barcode whitelist file "%s" could not be found.\n' "${bc_whitelist_filename}" >&2;
+    if [ ! -e "${tenx_atac_bc_whitelist_filename}" ] ; then
+        printf 'Error: Barcode whitelist file "%s" could not be found.\n' "${tenx_atac_bc_whitelist_filename}" >&2;
         return 1;
     fi
 
@@ -30,15 +30,15 @@ correct_barcode_in_fastq () {
     local first_barcode='';
 
     # Read first barcode from barcode whitelist file.
-    if [ "${bc_whitelist_filename%.gz}" == "${bc_whitelist_filename}" ] ; then
+    if [ "${tenx_atac_bc_whitelist_filename%.gz}" == "${tenx_atac_bc_whitelist_filename}" ] ; then
         # Uncompressed file.
-        read -r first_barcode < "${bc_whitelist_filename}";
+        read -r first_barcode < "${tenx_atac_bc_whitelist_filename}";
     else
         # Unset pipefail.
         set +o pipefail
 
         # Gzip compressed file.
-        first_barcode=$(zcat "${bc_whitelist_filename}" | head -n 1);
+        first_barcode=$(zcat "${tenx_atac_bc_whitelist_filename}" | head -n 1);
 
         # Set pipefail.
         set -o pipefail
@@ -49,29 +49,27 @@ correct_barcode_in_fastq () {
 
     # Check if the first barcode is not empty and only contains ACGT and no other characters.
     if [[ ${bc_length} -eq 0 || "${first_barcode}" != "${first_barcode//[^ACGT]/}" ]] ; then
-        printf 'Error: The first line of "%s" does not contain a valid barcode.\n' "${bc_whitelist_filename}";
+        printf 'Error: The first line of "%s" does not contain a valid barcode.\n' "${tenx_atac_bc_whitelist_filename}";
         return 1;
     fi
 
     # Get script dir.
     local script_dir="$(cd $(dirname "${BASH_SOURCE}") && pwd)";
 
-    # Correct barcodes in FASTQ file:
-    #   - Replace "type K = Kmer[16]" with the correct barcode length in the correct_barcode_in_fastq.seq script.
+    # Correct tagmentation and 10x ATAC barcodes in index 2 FASTQ file:
     seqc run \
-        -D bc_length="${bc_length}" \
         -release \
-        "${script_dir}/correct_barcode_in_fastq.seq" \
-            "${bc_whitelist_filename}" \
+        "${script_dir}/extract_and_correct_scalebio_atac_barcode_from_fastq.seq" \
+            "${tenx_atac_bc_whitelist_filename}" \
             "${fastq_with_raw_bc_filename}" \
             "/dev/stdout" \
-            "${fastq_with_corrected_bc_filename}.corrected.bc_stats.tsv" \
+            "${corrected_bc_filename}.corrected_bc_stats.tsv" \
             "${max_mismatches}" \
             "${min_frac_bcs_to_find}" \
       | pigz -p 4 \
-      > "${fastq_with_corrected_bc_filename}";
+      > "${corrected_bc_filename}";
 }
 
 
 
-correct_barcode_in_fastq "${@}";
+extract_and_correct_scalebio_atac_barcode_from_fastq "${@}";
