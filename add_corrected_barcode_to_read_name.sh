185cdc9745f137405fc9d5ec81e42512af04d3a0
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2023-01-09 11:42:43 +0100

    Change add_corrected_barcode_to_read_name.sh so it is able to handle trimmed R1 and R2 FASTQ files and a barcode correction file.
    Change add_corrected_barcode_to_read_name.sh so it is able to handle
    trimmed R1 and R2 FASTQ files and a barcode correction file as input
    where the barcode correction file was generated from the original
    barcode read (with the same number of reads than the untrimmed R1
    and R2 FASTQ files).

--- add_corrected_barcode_to_read_name.sh
+++ add_corrected_barcode_to_read_name.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2020-2021 - Gert Hulselmans
+# Copyright (C) 2020-2023 - Gert Hulselmans
 #
 # Purpose:
 #   Barcode 10x scATAC FASTQ files by adding the cell barcode from R2 to each
@@ -10,6 +10,7 @@
 decompress_fastq_cat_cmd='cat';
 decompress_fastq_zcat_cmd='zcat';
 decompress_fastq_igzip_cmd='igzip -c -d';
+decompress_fastq_zstd_cmd='zstd -c -d';
 
 
 # Number of threads to use to compress each FASTQ output file.
@@ -27,47 +28,33 @@ compress_fastq_gzip_cmd="gzip -${compress_fastq_level} -c";
 
 
 
-barcode_10x_scatac_fastqs () {
+add_corrected_barcode_to_read_name () {
     local fastq_R1_filename="${1}";
     local fastq_R2_filename="${2}";
-    local fastq_R3_filename="${3}";
+    local corrected_bc_filename="${3}";
     local fastq_output_prefix="${4}";
     local interleaved="${5:-false}";
-    local add_barcode_in_comment="${6:-true}";
-    local compress_fastq_cmd="${8:-pigz}";
+    local compress_fastq_cmd="${6:-pigz}";
 
     if [ ${#@} -lt 4 ] ; then
         printf '\nUsage:\n';
-        printf '    barcode_10x_scatac_fastqs \\\n';
+        printf '    add_corrected_barcode_to_read_name \\\n';
         printf '        fastq_R1 \\\n';
         printf '        fastq_R2 \\\n';
-        printf '        fastq_R3 \\\n';
+        printf '        corrected_bc_filename \\\n';
         printf '        fastq_output_prefix \\\n';
         printf '        <interleaved [true|false]> \\\n';
-        printf '        <add_barcode_in_comment [true|false]> \\\n';
-        printf '        <barcode_tags_or_separator> \\\n';
         printf '        <compress_fastq_cmd [bgzip|pigz|gzip|stdout|-|uncompressed]> \\\n\n';
-        printf 'Purpose: Barcode 10x scATAC FASTQ files by adding the cell barcode from R2 to each\n';
-        printf '         read in R1 and R3, as a comment or in front of the original read name.\n\n';
+        printf 'Purpose: Barcode scATAC R1 and R2 FASTQ files by adding raw barcode sequence, raw barcode quality\n';
+        printf '         and corrected barcode sequence in SAM tag format to each read name in R1 and R2 as comment.\n\n';
         printf 'Parameters:\n';
-        printf '  - fastq_R1:   FASTQ R1 filename (uncompressed or gzipped).\n';
-        printf '  - fastq_R2:   FASTQ R2 filename with barcodes (uncompressed or gzipped).\n';
-        printf '  - fastq_R3:   FASTQ R3 filename (uncompressed or gzipped).\n';
+        printf '  - fastq_R1:   FASTQ R1 filename.\n';
+        printf '  - fastq_R2:   FASTQ R2 filename.\n';
+        printf '  - corrected_bc_filename:   File with corrected barcodes.\n';
         printf '  - fastq_output_prefix: Output prefix for FASTQ output file(s).\n';
         printf '  - interleaved:\n';
-        printf '      - true:   Write one output FASTQ file with reads from R1 and R3 interleaved.\n';
-        printf '      - false:  Write R1 and R2 output FASTQ file with reads from R1 and R3 respectively (default).\n';
-        printf '  - add_barcode_in_comment:\n';
-        printf '      - true:   Add barcode and barcode quality from R2 as read comment in SAM spec format\n';
-        printf '                (for usage wtih: "bwa -C") (default).\n';
-        printf '      - false:  Add barcode from R2 at the beginning of the read (for usage with bap/sinto).\n';
-        printf '  - barcode_tags_or_separator:\n';
-        printf '      - If add_barcode_in_comment = "true":\n';
-        printf '          Specify barcode tag (2 characters) and barcode quality tag (2 characters)\n';
-        printf '          (default: "CR_CY").\n';
-        printf '      - If add_barcode_in_comment = "false":\n';
-        printf '          Specify string which will be added between barcode name and the original read name\n';
-        printf '          (default: "-").\n';
+        printf '      - true:   Write one output FASTQ file with reads from R1 and R2 interleaved.\n';
+        printf '      - false:  Write R1 and R2 output FASTQ file with reads from R1 and R2 respectively (default).\n';
         printf '  - compress_fastq_cmd:\n';
         printf '      - Compression program to use for output FASTQ files:\n';
         printf "          - \"bgzip\":  '%s'\n" "${compress_fastq_bgzip_cmd}";
@@ -82,36 +69,6 @@ barcode_10x_scatac_fastqs () {
         printf '          - export compress_fastq_threads="%s"\n\n' "${compress_fastq_threads}";
         printf '        To change compression level:\n';
         printf '          - export compress_fastq_level="%s"\n\n' "${compress_fastq_level}";
-        return 1;
-    fi
-
-
-    if [ "${add_barcode_in_comment}" = "true" ] ; then
-        local barcode_tags="${7:-CR_CY}";
-
-        if [ "${#barcode_tags}" -ne 5 ] ; then
-            printf 'Error: barcode tags field "%s" is wrongly formatted.\n' "${barcode_tags}" >&2;
-            return 1;
-        fi
-
-        # Extract barcode tag and barcode quality tag.
-        local barcode_tag="${barcode_tags:0:2}";
-        local barcode_qual_tag="${barcode_tags:3:2}";
-
-        local barcode_read_name_separator='';
-
-        # Redefine for mawk.
-        add_barcode_in_comment=1;
-    elif [ "${add_barcode_in_comment}" = "false" ] ; then
-        local barcode_read_name_separator="${7:--}";
-
-        local barcode_tag='';
-        local barcode_qual_tag='';
-
-        # Redefine for mawk.
-        add_barcode_in_comment=0;
-    else
-        printf 'Error: "add_barcode_in_comment" has an unsupported value: "%s".\n' "${add_barcode_in_comment}" >&2;
         return 1;
     fi
 
@@ -138,23 +95,30 @@ barcode_10x_scatac_fastqs () {
     fi
 
 
-    # Detect if input FASTQ files are gzip compressed or not.
+    # Detect if input FASTQ files are compressed (gzip/zstd) or not.
     if [ "${fastq_R1_filename}" != "${fastq_R1_filename%.gz}" ] ; then
         local decompress_R1_fastq_cmd="${decompress_fastq_gzipped_cmd}";
+    elif [ "${fastq_R1_filename}" != "${fastq_R1_filename%.zst}" ] ; then
+        local decompress_R1_fastq_cmd="${decompress_fastq_zstd_cmd}";
     else
         local decompress_R1_fastq_cmd="${decompress_fastq_cat_cmd}";
     fi
 
     if [ "${fastq_R2_filename}" != "${fastq_R2_filename%.gz}" ] ; then
         local decompress_R2_fastq_cmd="${decompress_fastq_gzipped_cmd}";
+    elif [ "${fastq_R2_filename}" != "${fastq_R2_filename%.zst}" ] ; then
+        local decompress_R2_fastq_cmd="${decompress_fastq_zstd_cmd}";
     else
         local decompress_R2_fastq_cmd="${decompress_fastq_cat_cmd}";
     fi
 
-    if [ "${fastq_R3_filename}" != "${fastq_R3_filename%.gz}" ] ; then
-        local decompress_R3_fastq_cmd="${decompress_fastq_gzipped_cmd}";
+    # Detect if input corrected barcode file is compressed (gzip/zstd) or not.
+    if [ "${corrected_bc_filename}" != "${corrected_bc_filename%.gz}" ] ; then
+        local decompress_corrected_bc_file_cmd="${decompress_fastq_gzipped_cmd}";
+    elif [ "${corrected_bc_filename}" != "${corrected_bc_filename%.zst}" ] ; then
+        local decompress_corrected_bc_file_cmd="${decompress_fastq_zstd_cmd}";
     else
-        local decompress_R3_fastq_cmd="${decompress_fastq_cat_cmd}";
+        local decompress_corrected_bc_file_cmd="${decompress_fastq_cat_cmd}";
     fi
 
 
@@ -183,113 +147,99 @@ barcode_10x_scatac_fastqs () {
     mawk \
         -v fastq_R1_filename="${fastq_R1_filename}" \
         -v fastq_R2_filename="${fastq_R2_filename}" \
-        -v fastq_R3_filename="${fastq_R3_filename}" \
+        -v corrected_bc_filename="${corrected_bc_filename}" \
         -v fastq_R1_output_filename="${fastq_R1_output_filename}" \
         -v fastq_R2_output_filename="${fastq_R2_output_filename}" \
-        -v barcode_tag="${barcode_tag}" \
-        -v barcode_qual_tag="${barcode_qual_tag}" \
-        -v barcode_read_name_separator="${barcode_read_name_separator}" \
         -v decompress_R1_fastq_cmd="${decompress_R1_fastq_cmd}" \
         -v decompress_R2_fastq_cmd="${decompress_R2_fastq_cmd}" \
-        -v decompress_R3_fastq_cmd="${decompress_R3_fastq_cmd}" \
+        -v decompress_corrected_bc_file_cmd="${decompress_corrected_bc_file_cmd}" \
         -v compress_fastq_cmd="${compress_fastq_cmd}" \
-        -v add_barcode_in_comment="${add_barcode_in_comment} " \
     '
     BEGIN {
         read_fastq_R1_cmd = decompress_R1_fastq_cmd " " fastq_R1_filename;
         read_fastq_R2_cmd = decompress_R2_fastq_cmd " " fastq_R2_filename;
-        read_fastq_R3_cmd = decompress_R3_fastq_cmd " " fastq_R3_filename;
+        read_corrected_bc_file_cmd = decompress_corrected_bc_file_cmd " " corrected_bc_filename;
 
         write_fastq_R1_cmd = compress_fastq_cmd " > " fastq_R1_output_filename;
         write_fastq_R2_cmd = compress_fastq_cmd " > " fastq_R2_output_filename;
 
-        fastq_line_number = 0;
+        read_name_corrected_bc = "";
+        corrected_bc_line = "";
 
-        # Read FASTQ R2 file (which contains the cell barcodes).
-        while ( (read_fastq_R2_cmd | getline fastq_R2_line) > 0 ) {
+        fastq_line_number = 0;
+        corrected_bc_line_number = 0;
+
+        # Read FASTQ R1 file (which contains read 1).
+        while ( (read_fastq_R1_cmd | getline fastq_R1_line) > 0 ) {
             fastq_line_number += 1;
             fastq_part = fastq_line_number % 4;
 
-            # Read FASTQ R1 file (which contains read 1).
-            if ( (read_fastq_R1_cmd | getline fastq_R1_line) > 0 ) {
-                # Read FASTQ R3 file (which contains read 2).
-                if ( (read_fastq_R3_cmd | getline fastq_R3_line) > 0 ) {
-                    if ( fastq_part == 1 ) {
-                        # Read name lines.
+            # Read FASTQ R2 file (which contains read 2).
+            if ( (read_fastq_R2_cmd | getline fastq_R2_line) > 0 ) {
+                if ( fastq_part == 1 ) {
+                    # Find first space position (0 if no comment found) in read name from all input FASTQ files.
+                    read_name_R1_space_pos = index(fastq_R1_line, " ");
+                    read_name_R2_space_pos = index(fastq_R2_line, " ");
 
-                        # Find first space position (0 if no comment found) in read name from all input FASTQ files.
-                        read_name_R1_space_pos = index(fastq_R1_line, " ");
-                        read_name_R2_space_pos = index(fastq_R2_line, " ");
-                        read_name_R3_space_pos = index(fastq_R3_line, " ");
+                    # Extract read name from all input FASTQ files.
+                    if (read_name_R1_space_pos > 0) {
+                        read_name_R1 = substr(fastq_R1_line, 2, read_name_R1_space_pos - 2);
+                    } else {
+                        read_name_R1 = substr(fastq_R1_line, 2);
+                    }
 
-                        # Extract read name from all input FASTQ files.
-                        if (read_name_R1_space_pos > 0) {
-                            read_name_R1 = substr(fastq_R1_line, 2, read_name_R1_space_pos - 2);
-                        } else {
-                            read_name_R1 = substr(fastq_R1_line, 2);
-                        }
+                    if (read_name_R2_space_pos > 0) {
+                        read_name_R2 = substr(fastq_R2_line, 2, read_name_R2_space_pos - 2);
+                    } else {
+                        read_name_R2 = substr(fastq_R2_line, 2);
+                    }
 
-                        if (read_name_R2_space_pos > 0) {
-                            read_name_R2 = substr(fastq_R2_line, 2, read_name_R2_space_pos - 2);
-                        } else {
-                            read_name_R2 = substr(fastq_R2_line, 2);
-                        }
+                    # Check if read names match between both FASTQ files.
+                    if ( read_name_R1 != read_name_R2 ) {
+                        print "Error: Read name R1 (\"" read_name_R1 "\") and read name R2 (\"" read_name_R2 "\") are not paired properly (line number: " fastq_line_number ").";
+                        exit(1);
+                    }
 
-                        if (read_name_R3_space_pos > 0) {
-                            read_name_R3 = substr(fastq_R3_line, 2, read_name_R3_space_pos - 2);
-                        } else {
-                            read_name_R3 = substr(fastq_R3_line, 2);
-                        }
+                    # Keep getting the next corrected barcode line till the read name matches with the
+                    # read name in the FASTQ files.
+                    # This allows to add corrected barcodes after quality trimming R1 and R2 FASTQ files,
+                    # while the barcode correction was done on the original barcode FASTQ file (which
+                    # can have more reads as none were removed as can happen during quality trimming).
+                    while ( read_name_corrected_bc != read_name_R1 ) {
+                        if ( (read_corrected_bc_file_cmd | getline corrected_bc_line) > 0 ) {
+                            corrected_bc_line_number += 1;
 
-                        # Check if read names match between all 3 FASTQ files.
-                        if ( read_name_R1 != read_name_R2 || read_name_R1 != read_name_R3 ) {
-                            print "Error: Read name R1 (\"" read_name_R1 "\"), read name R2 (\"" read_name_R2 "\") and R3 (\"" read_name_R3 "\") are not paired properly (line number: " fastq_line_number ").";
-                            exit(1);
-                        }
+                            # Split corrected barcode line on spaces:
+                            #   - read name: strip "@" from the start.
+                            #   - corrected barcode info as SAM tags separated by "\t":
+                            #       CR:Z:raw_bc\tCY:Z:raw_bc_qual\tCB:Z:corrected_bc_seq
 
-                        if (add_barcode_in_comment == 1) {
-                            # Store read name comments for R2 input FASTQ file so we can extract existing comments, like a corrected barcode.
-                            read_name_R2_comment = substr(fastq_R2_line, length(read_name_R2) + 3);
-                        } else {
-                            # Store full read names for R1 and R3 input FASTQ files.
-                            read_name_R1_full = substr(fastq_R1_line, 2);
-                            read_name_R3_full = substr(fastq_R3_line, 2);
-                        }
-                    } else if ( fastq_part == 2 ) {
-                        # Sequence lines.
+                            # Find first space position (0 if no comment found) in read name from all input FASTQ files.
+                            read_name_corrected_bc_space_pos = index(corrected_bc_line, " ");
 
-                        # Store sequence info from R1, R2 and R3 for later use.
-                        sequence_R1 = fastq_R1_line;
-                        sequence_R2 = fastq_R2_line;
-                        sequence_R3 = fastq_R3_line;
-                    } else if ( fastq_part == 0 ) {
-                        # Quality lines.
-
-                        if (add_barcode_in_comment == 1) {
-                            # Store barcode sequence and barcode quality in SAM spec format.
-                            read_name_comment = sprintf("%s:Z:%s\t%s:Z:%s", barcode_tag, sequence_R2, barcode_qual_tag, fastq_R2_line);
-
-                            # Split R2 read name comment on spaces and tabs so we can check if there are existing comments in SAM spec format.
-                            nbr_splits = split(read_name_R2_comment, read_name_R2_comment_array, / |\t/);
-
-                            for ( i = 1; i <= nbr_splits; i++ ) {
-                                if ( read_name_R2_comment_array[i] ~ /^[A-Za-z][A-Za-z]:[ABfHiZ]:/ ) {
-                                    # Add SAM spec comment from original barcode file.
-                                    read_name_comment = read_name_comment "\t" read_name_R2_comment_array[i];
-                                }
+                            # Extract read name from all input FASTQ files.
+                            if (read_name_corrected_bc_space_pos > 0) {
+                                read_name_corrected_bc = substr(corrected_bc_line, 2, read_name_corrected_bc_space_pos - 2);
+                                corrected_bc_sam_tags = substr(corrected_bc_line, read_name_corrected_bc_space_pos + 1);
+                            } else {
+                                read_name_corrected_bc = substr(corrected_bc_line, 2);
+                                corrected_bc_sam_tags = "";
                             }
-
-                            # Write the full FASTQ record to the R1 and R2 output FASTQ file with barcode info in the read name comments.
-                            # When write_fastq_R1_cmd and write_fastq_R2_cmd are the same, an interleaved FASTQ fille will be written.
-                            print "@" read_name_R1 " " read_name_comment "\n" sequence_R1 "\n+\n" fastq_R1_line | write_fastq_R1_cmd;
-                            print "@" read_name_R3 " " read_name_comment "\n" sequence_R3 "\n+\n" fastq_R3_line | write_fastq_R2_cmd;
-                        } else {
-                            # Write the full FASTQ record to the R1 and R2 output FASTQ file with barcode info in front of the read name.
-                            # When write_fastq_R1_cmd and write_fastq_R2_cmd are the same, an interleaved FASTQ fille will be written.
-                            print "@" sequence_R2 barcode_read_name_separator read_name_R1 "\n" sequence_R1 "\n+\n" fastq_R1_line | write_fastq_R1_cmd;
-                            print "@" sequence_R2 barcode_read_name_separator read_name_R3 "\n" sequence_R3 "\n+\n" fastq_R3_line | write_fastq_R2_cmd;
                         }
                     }
+                } else if ( fastq_part == 2 ) {
+                    # Sequence lines.
+
+                    # Store sequence info from R1 and R2 for later use.
+                    sequence_R1 = fastq_R1_line;
+                    sequence_R2 = fastq_R2_line;
+                } else if ( fastq_part == 0 ) {
+                    # Quality lines.
+
+                    # Write the full FASTQ record to the R1 and R2 output FASTQ file with barcode info in the read name comments.
+                    # When write_fastq_R1_cmd and write_fastq_R2_cmd are the same, an interleaved FASTQ fille will be written.
+                    print "@" read_name_R1 " " corrected_bc_sam_tags "\n" sequence_R1 "\n+\n" fastq_R1_line | write_fastq_R1_cmd;
+                    print "@" read_name_R2 " " corrected_bc_sam_tags "\n" sequence_R2 "\n+\n" fastq_R2_line | write_fastq_R2_cmd;
                 }
             }
         }
@@ -297,7 +247,7 @@ barcode_10x_scatac_fastqs () {
         # Close open file handles.
         close(read_fastq_R1_cmd);
         close(read_fastq_R2_cmd);
-        close(read_fastq_R3_cmd);
+        close(read_corrected_bc_file_cmd);
         close(write_fastq_R1_cmd);
         close(write_fastq_R2_cmd);
     }'
@@ -307,5 +257,5 @@ barcode_10x_scatac_fastqs () {
 
 
 
-barcode_10x_scatac_fastqs "${@}";
+add_corrected_barcode_to_read_name "${@}";
 
